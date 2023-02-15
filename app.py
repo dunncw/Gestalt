e9fa031cb86123da599899d220607a93938a3569
CaydenD <caydendunn2121@gmail.com>
2023-02-15 16:32:17 -0500

    reworked some ui elements

--- app.py
+++ app.py
@@ -29,42 +29,22 @@ llm = OpenAI(model_name="text-davinci-003", temperature=0, openai_api_key=os.env
 tools = load_tools(tool_names, llm=llm, news_api_key=news_api_key, tmdb_bearer_token=tmdb_bearer_token)
 agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
 
-# def set_openai_api_key(api_key, agent):
-#     if api_key:
-#         tool_names = ['python_repl', 'serpapi', 'wolfram-alpha', 'requests', 'terminal', 'pal-math', 'pal-colored-objects', 'llm-math', 'open-meteo-api', 'news-api', 'tmdb-api']
-
-#         # load in the api key and initialize gpt3
-#         llm = OpenAI(model_name="text-davinci-003", temperature=0, openai_api_key=os.environ["open_ai_api_key"])
-
-#         # # in prod this should look like this so we are taking the api key from the textbox(user input) and charging them for the api calls ### very important ###
-#         # os.environ["OPENAI_API_KEY"] = api_key
-#         # llm = OpenAI(model_name="text-davinci-003", temperature=0)
-#         # os.environ["OPENAI_API_KEY"] = ""
-
-#         tools = load_tools(tool_names, llm=llm, news_api_key=news_api_key, tmdb_bearer_token=tmdb_bearer_token)
-#         agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
-#         print(agent_state)
-#         return agent
-
 # Define chat function
-def chat(inp, history):
-    print("\n==== date/time: " + str(datetime.datetime.now()) + " ====")
-    print("inp: " + inp)
-    history = history or []
-    output = agent.run(inp)
-    history.append((inp, output))
-    return history, history
+def chat(user_input, chat_history):
+    # Print the user input and the date and time of the query
+    print("\n=-=-=-= date and time of query: " + str(datetime.datetime.now()) + " =-=-=-=")
+    print("user input: " + user_input)
+    # Append the user input and the date and time of the query to the chat history
+    chat_history = chat_history or []
+    agent_response = agent.run(user_input)
+    chat_history.append((user_input, agent_response))
+    # Return the chat history to the chatbot and the gradio state
+    return chat_history, chat_history
 
 # this is the code for the gradio interface
-block = gr.Blocks(css=".gradio-container {background-color: red}")
+block = gr.Blocks()
 
 with block:
-    with gr.Row():
-        gr.Markdown("<h3><center>LangChain AI</center></h3>")
-
-        # openai_api_key_textbox = gr.Textbox(placeholder="Paste your OpenAI API key (sk-...)",
-        #        show_label=False, lines=1, type='password')
-
     chatbot = gr.Chatbot()
 
     with gr.Row():
@@ -76,7 +56,7 @@ with block:
     gr.Examples(
         examples=["How many people live in Canada?",
                   "What is 13**.3?",
-                  "How much did it rain in SF today?",
+                  "what is the weather like today on the College of charleston Campus?",
                   "Get me information about the movie 'Avatar'",
                   "What are the top tech headlines in the US?",
                   "On the desk, you see two blue booklets, two purple booklets, and two yellow pairs of sunglasses - "
@@ -86,18 +66,10 @@ with block:
 
     gr.HTML("""<center>Powered by <a href='https://github.com/hwchase17/langchain'>LangChain 🦜️🔗</a></center>""")
 
-
     state = gr.State()
 
-
-    
     submit.click(chat, inputs=[message, state], outputs=[chatbot, state])
     message.submit(chat, inputs=[message, state], outputs=[chatbot, state])
-
-    # # if someone changes the API key text box, we set api key and initialize agent and load tools
-    # openai_api_key_textbox.change(set_openai_api_key,
-    #                             inputs=[openai_api_key_textbox, agent_state],
-    #                             outputs=[agent_state])
 
 block.queue()   # was launch(debug = True)
 
